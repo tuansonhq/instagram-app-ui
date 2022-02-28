5387750ea18bfa0a62ecb8defbff7e2acbf9fff8
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-02-28 15:49:36 +0700

    Update

---
+++ android/app/src/main/kotlin/com/example/instagramappui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.instagramappui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:instagramappui/pages/root_app.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: RootApp(),
+  ));
+}
+
+
